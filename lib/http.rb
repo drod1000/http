006729e4f159712e0080aecd073249407fbd1ec4
Daniel Rodriguez <drod2793@gmail.com>
2016-10-25 15:16:50 -0600

    cleaned up http

--- lib/http.rb
+++ lib/http.rb
@@ -27,15 +27,11 @@ class HTTP
 
       response = "<pre>" + "Hello, World! (#{counter})" + "</pre>"
       output = Messages.new.output(response)
-      # headers = ["http/1.1 200 ok",
-      #           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-      #           "server: ruby",
-      #           "content-type: text/html; charset=iso-8859-1",
-      #           "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+     
       client.puts Messages.new.headers(output)
       client.puts Messages.new.output(response)
 
-      # client.close
+      client.close
       @counter += 1
     end
   end
