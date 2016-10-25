b9e437cc2a92f5a7486ec1e888a1f1b71d030ecb
Daniel Rodriguez <drod2793@gmail.com>
2016-10-25 12:00:14 -0600

    Initial Setup

---
+++ request_reader.rb
@@ -0,0 +1,14 @@
+class RequestReader
+  attr_accessor :diagnostics
+  def initialize
+  @diagnostics = {"Verb" => nil,
+                  "Path" => nil,
+                  "Protocol" => nil,
+                  "Host" => nil,
+                  "Port" => nil,
+                  "Origin" => nil,
+                  "Accept" => nil
+  }
+  end
+  
+end
