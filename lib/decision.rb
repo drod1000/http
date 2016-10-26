0128132444e388ab6749fe65c9f040e5d90dc9b6
Daniel Rodriguez <drod2793@gmail.com>
2016-10-25 21:09:41 -0600

    Decision: response working

--- lib/decision.rb
+++ lib/decision.rb
@@ -1,2 +1,23 @@
 class Decision
+
+  attr_reader :path,
+              :count
+
+  def initialize(path, count)
+    @path = path
+    @count = count
+  end
+
+  def response
+    response = ""
+    if path == "/hello"
+      response = "<pre>" + "Hello, World! (#{count})" + "</pre>"
+    elsif path == "/datetime"
+      response = Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s
+    elsif path == "/shutdown"
+      response = "Total Requests: #{count}"
+    end
+    response
+  end
+
 end
--- test/decision_test.rb
+++ test/decision_test.rb
@@ -5,11 +5,23 @@ require './lib/decision'
 class DecisionTest < Minitest::Test
   
   def test_it_can_create_decision
-    decision = Decision.new
+    decision = Decision.new("something", 0)
     assert decision
   end
 
-  def test_it_can
+  def test_it_can_return_hello_string
+    expected = "<pre>Hello, World! (1)</pre>"
+    assert_equal expected, Decision.new("/hello", 1).response
   end
-  
+
+  def test_it_can_return_time_string
+    decision = Decision.new("/datetime", 1)
+    assert_equal Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s, decision.response
+  end
+
+  def test_it_can_return_shutdown_string
+    expected = "Total Requests: 1"
+    assert_equal expected, Decision.new("/shutdown", 1).response
+  end
+
 end
