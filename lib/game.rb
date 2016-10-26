e2fdcfe440f836dedc10bb9ada8c2180c3f9291d
Daniel Rodriguez <drod2793@gmail.com>
2016-10-26 17:39:16 -0600

    guesses started

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
 class Game
+
+  attr_reader :number
+
+  def initialize
+    @number = rand(0..100)
+    @guesses = 0
+  end
+
+  def guess_number(guess)
+  end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -4,9 +4,23 @@ require './lib/game'
 
 class GameTest < Minitest::Test
   
+  attr_reader :game
+
+  def setup
+    @game = Game.new
+  end
+
   def test_it_can_create_a_game
-    game = Game.new
     assert game
+  end
+
+  def test_it_initializes_with_random_number_and_guesses
+    assert game.number
+    assert game.guesses
+  end
+
+  def test_it_provides_feedback_for_low_number
+    expected = "You have made"
   end
 
 end
