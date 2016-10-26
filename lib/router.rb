35d9733c96fb05ac9410fd9c289309159d2435ee
Daniel Rodriguez <drod2793@gmail.com>
2016-10-26 12:57:05 -0600

    added diagnostics string to router

--- lib/router.rb
+++ lib/router.rb
@@ -18,7 +18,14 @@ class Router
       response = Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s
     elsif diagnostics["Path"] == "/shutdown"
       response = "Total Requests: #{count}"
+    else
+      response << "<pre>"
+      diagnostics.each do |key, value|
+        response << "#{key} : #{value}\n"
+      end
+        response << "</pre>"
     end
+
     response
   end
   
