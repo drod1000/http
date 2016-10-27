88cf718a9c6ad6a5d549b4398623f214d7019851
Daniel Rodriguez <drod2793@gmail.com>
2016-10-26 20:02:42 -0600

    Router Tests: All passing after changes to Router

--- test/router_test.rb
+++ test/router_test.rb
@@ -3,41 +3,39 @@ require 'minitest/pride'
 require './lib/router'
 
 class RouterTest < Minitest::Test
-  
+  attr_reader :router
+  def setup
+  @router = Router.new
+  end
   def test_it_can_create_router
-    router = Router.new({"Path"=>"/"}, 0)
     assert router
   end
 
   def test_it_can_return_hello_string
     expected = "<pre>Hello, World! (1)</pre>"
-    assert_equal expected, Router.new({"Path"=>"/hello"}, 1).response
+    assert_equal expected, router.response({"Path"=>"/hello"}, 1)
   end
 
   def test_it_can_return_time_string
-    router = Router.new({"Path"=>"/datetime"}, 1)
-    assert_equal Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s, router.response
+    assert_equal Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s, router.response({"Path"=>"/datetime"}, 1)
   end
 
   def test_it_can_return_shutdown_string
     expected = "Total Requests: 1"
-    assert_equal expected, Router.new({"Path"=>"/shutdown"}, 1).response
+    assert_equal expected, router.response({"Path"=>"/shutdown"}, 1)
   end
 
   def test_it_can_confirm_match_in_dictionary
-    router = Router.new({"Path"=>"/"}, 0)
     assert router.match_in_dictionary("apple")
     assert router.match_in_dictionary("strawberry")
   end
 
   def test_it_can_return_string_confirming_match_in_dictionary
-    router = Router.new({"Path"=>"/"}, 0)
     expected = "apple is a known word."
     assert_equal expected, router.word_search("apple")
   end
   
   def test_it_can_return_string_denying_match_in_dictionary
-    router = Router.new({"Path"=>"/"}, 0)
     expected = "aaaaa is not a known word."
     assert_equal expected, router.word_search("aaaaa")
   end
