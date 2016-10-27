dc86c86d0cae3cba9551383abdb23182fee37e26
Daniel Rodriguez <drod2793@gmail.com>
2016-10-27 07:41:13 -0600

    Messenger: Redirect Headers

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -11,5 +11,14 @@ class Messenger
   def output(response)
     "<html><head></head><body>#{response}</body></html>"
   end
+
+  def redirect_headers(output)
+    ["http/1.1 302",
+    "Location: http://127.0.0.1:9292/game",
+    "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+    "server: ruby",
+    "content-type: text/html; charset=iso-8859-1",
+    "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
  
 end
