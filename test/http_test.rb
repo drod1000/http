ef2a62dd5e11d96861036f9c084e62873ee9f361
Daniel Rodriguez <drod2793@gmail.com>
2016-10-27 06:44:49 -0600

    HTT Test: Faraday fixed

--- test/http_test.rb
+++ test/http_test.rb
@@ -6,6 +6,9 @@ require 'pry'
 
 class HTTPTest < Minitest::Test
 
+  def self.test_order
+    :alpha
+  end
   def test_it_initializes_with_server_and_counter
     http = HTTP.new 
     assert_equal TCPServer, http.tcp_server.class 
@@ -15,7 +18,52 @@ class HTTPTest < Minitest::Test
   def test_faraday_works
     response = Faraday.get("http://127.0.0.1:9292/")
     assert_equal 200, response.status
-    assert_equal "Hello World", response.body
+ end
+
+ def test_hello_world
+  response = Faraday.get("http://127.0.0.1:9292/hello")
+  assert_equal "<html><head></head><body><pre>Hello, World! (2)</pre></body></html>", response.body
+  response = Faraday.get("http://127.0.0.1:9292/hello")
+  assert_equal "<html><head></head><body><pre>Hello, World! (3)</pre></body></html>", response.body
+  response = Faraday.get("http://127.0.0.1:9292/hello")
+  assert_equal "<html><head></head><body><pre>Hello, World! (4)</pre></body></html>", response.body
+ end
+
+ def test_diagnostics
+  response = Faraday.get("http://127.0.0.1:9292/")
+  assert response.body
+  end
+ 
+ def test_date_time
+ response = Faraday.get("http://127.0.0.1:9292/datetime")
+ time = Time.now.strftime('%e %b %Y %H:%M:%S%p').to_s
+ assert response.body.include?(time)
+ end
+
+ def test_it_can_extract_parameters
+  response = Faraday.get("http://127.0.0.1:9292/?word=hello")
+  assert response.body.include?("Parameter : word")
+  assert response.body.include?("Value : hello")
+ end
+
+ def test_it_can_confirm_known_word
+  response = Faraday.get("http://127.0.0.1:9292/word_search?word=hello")
+  expected = "is a known word"
+  assert response.body.include?(expected)
+  response_2 = Faraday.get("http://127.0.0.1:9292/word_search?word=apple")
+  expected_2 = "is a known word."
+  assert response.body.include?(expected_2)
+ end
+
+ def test_it_can_refute_unknown_word
  end
  
+
+ def test_shutdown
+ response = Faraday.get("http://127.0.0.1:9292/shutdown")
+ assert response.body.include?("Total Requests:")
+ end
+
+
+
 end
